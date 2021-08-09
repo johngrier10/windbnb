4b19f2b5f676446bb53d37b0888950aa9f09fb96
John <you@example.com>
2021-08-09 15:32:05 -0400

    fixed logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div className="App-header">
-                
-      <a href="/">
-        <span class="material-icons md-48">face</span><p>WindBNB</p>
-      </a>
-       
-        </div>
+        <header>
+          <div className="App-header">
+                  
+                <a href="/">
+          <span class="material-icons md-48">face</span><p>WindBNB</p>
+                </a>
+          </div>
+        </header>
     )
 }
 
